671bc48d9a3eec385f71c5b9ec7b2ef12a870131
Ruben <rzubicoa@eservicios.indra.es>
2025-12-03 10:55:12 +0100

    feat: mejorar los estilos de la tabla

--- src/app/orders/orders.routes.ts
+++ src/app/orders/orders.routes.ts
@@ -4,8 +4,5 @@ export const ordersRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/orders/orders.component').then(m => m.OrdersComponent),
-    data: {
-      breadcrumb: 'Pedidos',
-    },
   },
 ];
