f061f3027a33b88c92e20e7027f5fc0ca0e21561
Ruben <rzubicoa@eservicios.indra.es>
2026-01-11 10:40:06 +0100

    feat: agregar filtros a los productos

--- src/app/products/components/products-filters/products-filters.component.ts
+++ src/app/products/components/products-filters/products-filters.component.ts
@@ -1,28 +1,33 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { ProductsFormFiltersService } from '../../services/products-form-filters.service';
 import { CommonModule } from '@angular/common';
 import { FiltersContainerComponent } from '../../../shared/components/filters-container/filters-container.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { ProductFilters } from '../../../core/models/Product';
 import { PRODUCTS_CHIPS } from '../../models/products-chips';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-products-filters',
-  imports: [CommonModule, FiltersContainerComponent, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule],
+  imports: [CommonModule, FiltersContainerComponent, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatSliderModule, MatSlideToggleModule, FormsModule],
   templateUrl: './products-filters.component.html',
   styleUrl: './products-filters.component.scss'
 })
 export class ProductsFiltersComponent {
   public readonly formService = inject(ProductsFormFiltersService);
   
+  public maxPrice = input<number>(100);
   public applyFilters = output<ProductFilters>(); 
 
   public filters: ProductFilters | undefined = undefined;
   public readonly chips = PRODUCTS_CHIPS;
+  public startPrice = 0;
+  public endPrice = 100;
 
   ngOnInit(): void {
     this.filters = this.formService.obtenerDatos(this.formService.form);
@@ -30,6 +35,10 @@ export class ProductsFiltersComponent {
 
   applyFiltersClick() {
     this.filters = this.formService.obtenerDatos(this.formService.form);
+    this.filters.price = {
+      start: this.startPrice,
+      end: this.endPrice,
+    };
     this.applyFilters.emit(this.filters);
   }
 
@@ -45,5 +54,9 @@ export class ProductsFiltersComponent {
       this.formService.actualizarFormulario(this.formService.form, filters);
       this.applyFiltersClick();
     }
+  }
+
+  priceChange(event: Event) {
+    console.log(event);
   }
 }
